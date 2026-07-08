a6a6f68db39742f888b71d6404e57cb37e726948
dakmalandro <dakmalandro86@gmail.com>
2026-07-09 01:10:57 +0300

    Refactor code structure for improved readability and maintainability

---
+++ lib/fonts.ts
@@ -0,0 +1,19 @@
+import { Montserrat } from "next/font/google";
+import localFont from "next/font/local";
+
+export const montserrat = Montserrat({
+  variable: "--font-montserrat",
+  subsets: ["latin"],
+});
+
+export const brittany = localFont({
+  src: "../public/fonts/BrittanySignatureScript.ttf",
+  variable: "--font-brittany",
+  display: "swap",
+});
+
+export const veneer = localFont({
+  src: "../public/fonts/VeneerTwo.ttf",
+  variable: "--font-veneer",
+  display: "swap",
+});
